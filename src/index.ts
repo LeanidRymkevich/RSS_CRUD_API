5bdc077fd0a30395d033416dea4a09a3df0b394c
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-02-10 18:31:43 +0300

    fix: fix problem with tests compiling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
+import { multiply } from './folder/multiply';
 
 console.log(process.env.PORT);
 
-export const sum = (a: number, b: number) => a + b;
+export const sum = (a: number, b: number) => multiply(a, b) + a;
+
+console.log(multiply(2, 4));
